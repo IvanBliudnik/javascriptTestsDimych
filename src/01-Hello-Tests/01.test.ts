7bdd33a3491271ef4c818d8a8628e7ab788f2fc5
IvanBliudnik <Bliudnik007@gmail.com>
2024-06-09 13:14:20 +0300

    first commit

---
+++ src/01-Hello-Tests/01.test.ts
@@ -0,0 +1,10 @@
+export function splitIntoWords(sentence:string) {
+    return ["Hello", "my", "Friends"]
+}
+
+export function sum(a:number, b:number) {
+    return a + b
+}
+export function mult(a:number, b:number) {
+    return a * b
+}
