4f149b2cc5a79ba9421df9ab7e9ffc63cfd8d569
IvanBliudnik <Bliudnik007@gmail.com>
2024-06-09 20:24:42 +0300

    1 week tests

---
+++ src/02-Object/02-02.ts
@@ -0,0 +1,28 @@
+export type StreetType = {
+    title:string,
+}
+
+export type AdressType = {
+    number?:number,
+    street: StreetType,
+}
+
+export type HouseType = {
+    buildedAt:number,
+    repaired:false,
+    address:AdressType,
+}
+
+export type governmentBuildingsType = {
+    type: "HOSPITAL" | "FIRE-STATION",
+    budget: number,
+    staffCount: number,
+    address: AdressType, //потому что по свойствам подходит по запросу
+}
+
+export type CityType = {
+    title: string,
+    houses: Array<HouseType>,
+    governmentBuildings: Array<governmentBuildingsType>,
+    citizenNumber: number,
+}
